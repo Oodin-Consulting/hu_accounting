4218ca36d44b9612cbe2626e64542e790e524b13
atta-t <atta@nvm.ro>
2015-02-23 15:49:15 +0200

    hu_account_base module index

--- hu_account_base/__openerp__.py
+++ hu_account_base/__openerp__.py
@@ -38,7 +38,13 @@ Accounting:
  - Advance invoicing
  - Linked Invoices
  - Cash paid invoices - Rounding
-* 
+
+*** A számlázómodul használatához a jogszabályoknak megfelelően, igazolni kel az adóalanyiságot az adószám megadásával a regisztrációs felületünkön ( a regisztrációhóz kattintson ide ) , valamit be kell jelenteni a NAV felé a számlázómodul használatának megkezdését a SZAMLAZO nyomtatvány SZÁMLÁZÓ PROGRAM fejezetének kitöltésével. 
+Köszönettel: Oodin Consulting
+
+ To use our hungarian invoicing module according to hungarian law, you have to verify your hungarian tax number on our regisration form (click here). You also have to register the usage of our accounting modulethis module to NAV, by filling the SZAMLAZO at the SZÁMLÁZÓ PROGRAM chapter.
+Thank you: Oodin Consulting ***
+
     """,    
     "data" : [              
               'data/report_paperformat.xml',
